6639d131a7542622ca85e9e58c4730e4f0ae282e
Adeja Lendor <alendor6@gmail.com>
2019-08-31 01:31:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,10 +1,7 @@
 def map(source_array) 
-base = (new_array = []
+new_array = []
 i = 0
-  while i < source_array.length)
-  yield
-  base 
-  do
+while i < source_array.length do
   new_array.push(source_array[i] * -1)
   while i < source_array.length do
   new_array.push(source_array[i])
