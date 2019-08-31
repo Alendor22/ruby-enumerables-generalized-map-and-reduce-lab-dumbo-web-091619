ae91008f28795e7dc8bd9b5285b2b0fa24ea2ffd
Adeja Lendor <alendor6@gmail.com>
2019-08-31 01:23:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,9 +5,8 @@ i = 0
   new_array.push(source_array[i] * -1)
   array1.push(source_array[i])
   array1.push(source_array[i] * 2)
-  
+  array1.push(source_array[i] ** 2)
 i+= 1
 end
   new_array
-  source_array
 end
