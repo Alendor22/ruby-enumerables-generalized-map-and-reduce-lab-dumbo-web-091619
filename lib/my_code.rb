cfb66eda9d74f6a2202fd1107f482547a73a1cbe
Adeja Lendor <alendor6@gmail.com>
2019-08-31 01:12:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,7 @@
 def map(source_array, new_array, element1, element2)
-  new_array = Array.new
-  element1 = while i < source_array.length
+  yield(new_array = Array.new
+  element1 = while i < source_array.length)
+end
   element2 = i+= 1
 end
   #{element3}
