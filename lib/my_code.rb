ab3fcbd84ec352687b55a2d9ba060c5f8ce0b732
Adeja Lendor <alendor6@gmail.com>
2019-08-31 01:25:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,9 +3,12 @@ new_array = []
 i = 0
   while i < source_array.length do
   new_array.push(source_array[i] * -1)
-  array1.push(source_array[i])
-  array1.push(source_array[i] * 2)
-  array1.push(source_array[i] ** 2)
+  while i < source_array.length do
+  new_array.push(source_array[i])
+  while i < source_array.length do
+  new_array.push(source_array[i] * 2)
+  while i < source_array.length do
+  new_array.push(source_array[i] ** 2)
 i+= 1
 end
   new_array
