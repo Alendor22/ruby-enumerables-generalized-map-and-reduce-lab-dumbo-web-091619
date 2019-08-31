6890383ab73014d25fd6338f9382e6cea43367cd
Adeja Lendor <alendor6@gmail.com>
2019-08-31 00:22:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,10 @@
-element1 = 
-def map_negativized(source_array, element1 = , element2 = i+= 1)
- base = 
+element1 = array.new
+element2 = {while i < source_array.length}
+element3 = {i+= 1}
+def map_negativized(source_array, element1, element2)
+ #{element1}
+ #{element2}
  do
   array1.push(source_array[i] * -1)
-
+#{element3}
 end
