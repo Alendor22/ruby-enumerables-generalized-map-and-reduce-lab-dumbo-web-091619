455461bc81939b3fc1a438b987f2fcf9398965f9
Adeja Lendor <alendor6@gmail.com>
2019-08-31 00:32:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,8 @@
 element1 = array.[]
 element2 = while i < source_array.length
 element3 = i+= 1
-def map_negativized(source_array, element1, element2, element3)
- #{element1}
- #{element2}
+def map(source_array, element1, element2, element3)
+
  do
   array.push(source_array[i] * -1)
 end
