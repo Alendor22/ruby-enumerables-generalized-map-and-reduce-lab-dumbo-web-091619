b9aa90685ab34cacaab128a5c37ebde8ade834c5
Adeja Lendor <alendor6@gmail.com>
2019-08-31 01:07:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,5 @@
 def map(source_array, element1, element2, element3)
-  element1 = ar
+  element1 = Array
   element2 = while i < source_array.length, element3 = i+= 1)
   base = do #{element1}
  #{element2}
