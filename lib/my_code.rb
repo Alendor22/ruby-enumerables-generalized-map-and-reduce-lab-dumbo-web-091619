ef7b3401d6381db70ab5560e2804a47deac4932e
Adeja Lendor <alendor6@gmail.com>
2019-08-31 01:05:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,6 @@
-def map(source_array, element1 = array.[], element2 = while i < source_array.length, element3 = i+= 1)
+def map(source_array, element1, element2, element3)
+  element1 = ar
+  element2 = while i < source_array.length, element3 = i+= 1)
   base = do #{element1}
  #{element2}
 end
