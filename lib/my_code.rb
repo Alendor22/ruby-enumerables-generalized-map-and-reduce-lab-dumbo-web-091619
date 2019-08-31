76eee4d1bc4e186c9a3bbe16153b4f05b845cad4
Adeja Lendor <alendor6@gmail.com>
2019-08-31 00:43:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,8 @@
 def map(source_array, element1 = array.[], element2 = while i < source_array.length, element3 = i+= 1)
 
- do
-  array.push(source_array[i] * -1)
+ 
 end
-return element1
+
+do
+  array.push(source_array[i] * -1)
 end
