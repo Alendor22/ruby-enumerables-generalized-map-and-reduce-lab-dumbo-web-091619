6456164b91c15bbc4172cf519290d4ec10874bb0
Adeja Lendor <alendor6@gmail.com>
2019-08-31 01:56:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,17 +1,8 @@
-def my_own_map(source_array, a = (source_array[i] * -1), b = (source_array[i]), c = ) 
-new_array = []
+def my_own_map(source_array, a = (source_array[i] * -1), b = (source_array[i]), c = (source_array[i] * 2), d (source_array[i] ** 2)) 
+base == new_array = []
 i = 0
 while i < source_array.length do
   new_array.push
-  i+= 1
-  while i < source_array.length do
-  new_array.push(source_array[i])
-  i+= 1
-  while i < source_array.length do
-  new_array.push(source_array[i] * 2)
-  i+= 1
-  while i < source_array.length do
-  new_array.push(source_array[i] ** 2)
   i+= 1
 end
   new_array
